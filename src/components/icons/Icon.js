c8ac441d37cf815f93b528fd8042d4ebc612ce7a
JoaquinGabriel17 <joaquingabriel3@hotmail.com>
2025-10-17 15:16:59 -0300

    ajustes en estilos

--- src/components/icons/Icon.js
+++ src/components/icons/Icon.js
@@ -7,43 +7,30 @@ import { FaDownload } from 'react-icons/fa';
 
 export default function Icon(){
 
-    const [copiado, setCopiado] = useState('Haz click para copiar la información');
 
     const copiar = (texto) => {
-      navigator.clipboard.writeText(texto).then(() => {
-        setCopiado(`Copiado: ${texto}`);
-        setTimeout(() => setCopiado('Click para copiar la información'), 2000);
-      });
+      navigator.clipboard.writeText(texto)
     };
 
     return(
       <div className={styles.container}>
         <div className={styles.iconContainer}>
-            <a>
+            <a href='https://github.com/JoaquinGabriel17' target="_blank" rel="noopener noreferrer">Github
         <FaGithub 
         className={styles.icon}
-        onClick={() => {copiar('https://github.com/JoaquinGabriel17')}}
         ></FaGithub>
       </a>
-      <a>
+      <a href='https://www.linkedin.com/in/joaquin-ocampo-a7b213252/' target="_blank" rel="noopener noreferrer">Linkedin
         <FaLinkedin 
         className={styles.icon}
-        onClick={() => {copiar('https://www.linkedin.com/in/joaquin-ocampo-a7b213252/')}}
         ></FaLinkedin>
       </a>
-      <a>
-        <FaWhatsapp 
-        className={styles.icon}
-        onClick={() => {copiar('+54 9 3876 56-7092')}}
-        ></FaWhatsapp>
-      </a>
-      <a>
+      <a onClick={() => {copiar('joaquingabriel3@hotmail.com')}}>joaquingabriel3@hotmail.com
         <FaEnvelope 
         className={styles.icon}
-        onClick={() => {copiar('joaquingabriel3@hotmail.com')}}
+        
         ></FaEnvelope>
       </a>
-      <p className={styles.tooltip}>{copiado}</p>
         </div>
         <a
       href="/cv.docx" download >
