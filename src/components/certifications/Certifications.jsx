750295e5c73eb57f59241d0e2960bd785eda633d
JoaquinGabriel17 <joaquingabriel3@hotmail.com>
2026-07-20 23:53:55 -0300

    certification styles change

--- src/components/certifications/Certifications.jsx
+++ src/components/certifications/Certifications.jsx
@@ -13,22 +13,23 @@ export default function Certifications({ data , lang}) {
                                 {category.certifications.map((certification, certIndex) => {
                                     return(
                                         <div key={certIndex} className={styles.certification}>
-                                            {certification.image && <img src={certification.image} alt={certification.name} />}
-                                            <div>
+                                            
+                                            <div className={styles.dataContainer}>
                                                 <div className={styles.certificationHeader}>
                                                     <p className={styles.certificationCompany}>{certification.company}</p>
                                                     <p className={styles.certificationDate}>{certification.date}</p>
                                                 </div>
                                                 <p className={styles.certificationName}>{certification.name}</p>
                                                 <div className={styles.certificationLinks}>
-                                                    <a className={styles.certificationLink} href={certification.url} target="_blank" rel="noopener noreferrer">
+                                                    {certification.url !== null && <a disabled={certification.url !== null} className={styles.certificationLink} href={certification.url} target="_blank" rel="noopener noreferrer">
                                                         {lang === "es" ? "Ver Certificación" : "View Certification"}
-                                                    </a>
+                                                    </a>}
                                                     <a className={styles.certificationLink} href={certification.pdf} download target="_blank" rel="noopener noreferrer">
                                                         {lang === "es" ? "Descargar PDF" : "Download PDF"}
                                                     </a>
                                                 </div>
                                             </div>
+                                            {certification.image && <img src={certification.image} alt={certification.name} />}
                                         </div>
                                     )
                                 })}
