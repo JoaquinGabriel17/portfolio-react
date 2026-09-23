4a613c5bcd17e78d0b049b2ca1ba3c01c4ef2173
JoaquinGabriel17 <joaquingabriel3@hotmail.com>
2026-09-22 22:18:56 -0300

    remplazar color para acento

--- src/components/experienceCard/ExperienceCard.jsx
+++ src/components/experienceCard/ExperienceCard.jsx
@@ -18,7 +18,7 @@ export default function ExperienceCard({ data, lang }) {
                 </div>
                 <h3 className={styles.companyName}>{company}</h3>
                 <div className={styles.tagsContainer}>
-                    {/* El primer tag (rol) va en color índigo */}
+                    {/* El primer tag (rol) va en color rojo */}
                     <span className={`${styles.tag} ${styles.primaryTag}`}>{role}</span>
                     {tags.map((tag, index) => (
                         <span key={index} className={styles.tag}>{tag}</span>
