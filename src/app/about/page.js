600784a5488209f59f786892948eb83946017359
JoaquinGabriel17 <joaquingabriel3@hotmail.com>
2026-09-23 11:41:55 -0300

    quitar margin top para poner margin bottom

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,6 +1,10 @@
+import Navbar from "@/components/navbar/Navbar"
+import styles from "./about.module.css"
+
 export default function About() {
     return(
-        <div>
+        <div className={styles.main}>
+            <Navbar></Navbar>
             <h1>About</h1>
         </div>
     )
